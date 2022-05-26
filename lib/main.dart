89dd9f1d6c1aec16620bb0c37aa2c2d66892f1db
Ian Mukunya <ianmukunya@gmail.com>
2022-05-26 10:47:44 +0300

    Widgets - Stateless and Stateful

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'introToWidgets/widgets.dart';
+import 'introToWidgets/StatefulAndStatelessWidgets.dart';
+void main() {
+  runApp(const MyApp());
+}
